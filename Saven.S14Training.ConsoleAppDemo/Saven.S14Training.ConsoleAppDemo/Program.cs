24662a820e55ecbad7a8c7945266c75d8bab21a0
Mallinadh <jmalli007@gmail.com>
2013-12-06 16:48:37 +0530

    Add project

---
+++ Saven.S14Training.ConsoleAppDemo/Saven.S14Training.ConsoleAppDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+
+namespace Saven.S14Training.ConsoleAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name);
+            Console.WriteLine("Console Application Demo");
+
+#if TRACE
+            Trace.WriteLine("This is trace message..");
+#endif 
+#if DEBUG
+            Debug.WriteLine("This is debug message");
+#endif 
+            Console.ReadLine();
+        }
+    }
+}
